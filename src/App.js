306867a947fb2ac311b4ad560a724fbc96d38262
Alan Rodrigo Albert Moran <alanalbertmn@gmail.com>
2025-03-05 23:59:50 -0600

    Removed titles when empty arrays and made app responsive

--- src/App.js
+++ src/App.js
@@ -11,49 +11,49 @@ export const App = () => {
 	const search = e => {
 		e.preventDefault();
 		const url = `https://spotify23.p.rapidapi.com/search/?q=${input.current.value}`;
-		console.log(url);
 		axios
 			.get(url, {
 				headers: {
 					'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_KEY,
-                    'X-RapidAPI-Host': process.env.REACT_APP_RAPIDAPI_HOST,
+					'X-RapidAPI-Host': process.env.REACT_APP_RAPIDAPI_HOST,
 				},
 			})
 			.then(response => {
 				setAlbums(response.data.albums.items);
 				setArtists(response.data.artists.items);
 			});
-	};
-
-	console.log(albums);
-	console.log(artists);
-
+	}
 
 	return (
 		<form onSubmit={search}>
 			<div className='container'>
-				<div className='flex center' style={{gap: 15}}>
-				<FaSpotify size={80} color='#1DB954'/>
-				<h1 style={{fontSize: 60, fontWeight: 'bolder'}}>Spotify Service</h1>
+				<div className='flex center' style={{ gap: 15 }}>
+					<FaSpotify size={80} color='#1DB954' />
+					<h1 id='title'>Spotify Service</h1>
 				</div>
 				<div id='div-busqueda'>
 					<input
 						id='busqueda'
-						placeholder='Introduzca su búsqueda'
+						placeholder='Enter an album or an artist here...'
 						formAction='submit'
 						ref={input}
 						autoComplete="off"
 					/>
 				</div>
-				<h1 className='title'>Albums</h1>
-				<div className='albums'>
-					{albums && albums.map(el => <Album data={el.data} />)}
-				</div>
-				<hr className='separator'/>
-				<h1 className='title'>Artists</h1>
-				<div className='albums'>
-					{artists && artists.map(el => <Artist data={el.data} />)}
-				</div>
+				{console.log(albums)}
+				{albums.length>0 && (
+					<>
+						<h1 className='title'>Albums</h1>
+						<div className='albums'>
+							{albums && albums.map(el => <Album data={el.data} />)}
+						</div>
+						<hr className='separator' />
+						<h1 className='title'>Artists</h1>
+						<div className='albums'>
+							{artists && artists.map(el => <Artist data={el.data} />)}
+						</div>
+					</>
+				)}
 			</div>
 		</form>
 	);
